16f254f35dcc7ffc8265142bd181a0e801c85971
Arthur Ussuy Ferrari <rf67cyr4sw@privaterelay.appleid.com>
2026-01-20 20:23:00 -0300

    Refact/code smells (#3)
    refact: remoçao de código desnecessário

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { LoginComponent } from './features/auth/login/login.component';
 import { ForgotPasswordComponent } from './features/auth/forgot-password/forgot-password.component';
 import { AuthenticatedLayoutComponent } from './features/shell/authenticated-layout/authenticated-layout.component';
 import { authGuard } from './core/guards/auth.guard';
-import { HomeComponent } from './features/dashboard/home/home.component';
 import { StatementComponent } from './features/dashboard/statement/statement.component';
 import { MonthlyContributionComponent } from './features/dashboard/monthly-contribution/monthly-contribution.component';
 import { AdditionalContributionComponent } from './features/dashboard/additional-contribution/additional-contribution.component';
@@ -25,11 +24,8 @@ export const routes: Routes = [
     {
         path: 'app',
         component: AuthenticatedLayoutComponent,
+        canActivate: [authGuard],
         children: [
-            {
-                path: '',
-                component: HomeComponent
-            },
             {
                 path: 'statement',
                 component: StatementComponent
@@ -61,7 +57,8 @@ export const routes: Routes = [
             {
                 path: 'information',
                 component: InformationComponent
-            }
+            },
+            { path: '', redirectTo: 'monthly-contribution', pathMatch: 'full' },
         ]
     },
     {
