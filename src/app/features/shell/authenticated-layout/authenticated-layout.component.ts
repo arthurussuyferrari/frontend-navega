1d839a8fe47174384488cbe23f541302d4211405
Arthur Ussuy Ferrari <rf67cyr4sw@privaterelay.appleid.com>
2026-01-21 19:02:40 -0300

    feat: criação do componente de header (#6)

--- src/app/features/shell/authenticated-layout/authenticated-layout.component.ts
+++ src/app/features/shell/authenticated-layout/authenticated-layout.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+
 @Component({
   selector: 'app-authenticated-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: `
-    <div>
-      <aside>MENU</aside>
-      <main>
-        <router-outlet />
-      </main>
-    </div>
-  
-  `,
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  templateUrl: './authenticated-layout.component.html',
   styleUrls: ['./authenticated-layout.component.less']
 })
 export class AuthenticatedLayoutComponent {
