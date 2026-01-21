c38ec48417779bcac5890e9c19562f62afee8755
arthurussuyferrari <rf67cyr4sw@privaterelay.appleid.com>
2026-01-21 17:54:52 -0300

    feat: criação do componente de button

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule, MatButtonModule],
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.less']
+})
+export class ButtonComponent {
+  @Input() text!: string;
+  @Input() type!: string;
+}
