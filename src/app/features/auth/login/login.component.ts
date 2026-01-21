e9b03324f729958f1d0a7531b96766867dea074e
arthurussuyferrari <rf67cyr4sw@privaterelay.appleid.com>
2026-01-21 17:56:17 -0300

    feat: criação da tela de login

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { BackgroundComponent } from './../../../shared/components/background/background.component';
+import { InputComponent } from './../../../shared/components/input/input.component';
+import { InputPasswordComponent } from './../../../shared/components/input-password/input-password.component';
+import { ButtonComponent } from './../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, BackgroundComponent, InputComponent, InputPasswordComponent,ButtonComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.less']
 })
